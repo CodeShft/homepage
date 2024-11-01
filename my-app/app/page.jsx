49ec7bc4fdcb862b724478ae0916aa78981622e4
CodeShft <nileceyazilim@gmail.com>
2024-11-01 22:17:47 +0300

    bug fix

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -10,14 +10,17 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Cat</span>
             <h1 className="h1 mb-6  text-amber-700">
-              Hello I'm Lokum <br />
+              Hello I am Lokum <br />
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I am a playful cat, I will be a very good friend to you, I love
               dried cat food and sleep. I also get along well with other cats.
             </p>
           </div>
-          <div className="order-1 xl:order-none mb-8 xl:mb-0"> <Photo/></div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            {" "}
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
