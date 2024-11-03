b9793bb00a19e011e4a4c2ce6aafc4601368f746
CodeShft <nileceyazilim@gmail.com>
2024-11-03 20:39:07 +0300

    new features added

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
 
-const resume = () => {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
+import { motion } from "framer-motion";
+const about = {
+  title: "About me",
+  description: "Lorem ipsum",
+
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Lokum",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Turkish Tekir cat",
+    },
+  ],
+};
+
+export default function Resume() {
   return (
-    <div>resume page</div>
-  )
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="name"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="name">Name</TabsTrigger>
+            <TabsTrigger value="nationality">Nationality</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+          {/* container */}
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="name" className="w-full">
+              name
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 }
-
-export default resume
