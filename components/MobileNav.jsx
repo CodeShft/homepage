9f8c924c0502c1768285e8aa603afb5be53f46a2
CodeShft <nileceyazilim@gmail.com>
2024-11-12 18:53:05 +0300

    changed fixed

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,30 +1,20 @@
-import { Sheet, SheetContent, SheetTrigger, SheetTitle } from "@/components/ui/sheet";
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 import { useState } from "react";
 
 const links = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "Features",
-    path: "/features",
-  },
-  {
-    name: "Work",
-    path: "/work",
-  },
-  {
-    name: "Resume",
-    path: "/resume",
-  },
+  { name: "Home", path: "/" },
+  { name: "Features", path: "/features" },
+  { name: "Work", path: "/work" },
+  { name: "Resume", path: "/resume" },
 ];
 
 const MobileNav = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // State to track if the sheet is open
   const pathname = usePathname();
 
   // Function to close the sheet when a link is clicked
@@ -36,30 +26,26 @@ const MobileNav = () => {
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger
         className="flex justify-center items-center"
-        onClick={() => setIsOpen(true)}
+        onClick={() => setIsOpen(true)} // Open the sheet when the hamburger menu is clicked
       >
         <CiMenuFries className="text-[32px] text-amber-700" />
       </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        {/* Title for accessibility */}
-        <SheetTitle className="sr-only">Mobil Menü</SheetTitle>
-
-        {/* Logo and home page redirection */}
+      <SheetContent className="flex flex-col px-4 py-8">
+        {/* Logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/" onClick={handleLinkClick}> {/* Close the menu on logo click */}
+          <Link href="/" onClick={handleLinkClick}>
             <h1 className="text-4xl font-semibold">
               Lokum<span className="text-amber-700">.</span>
             </h1>
           </Link>
         </div>
-
-        {/* Navigation links */}
+        {/* Navigation */}
         <nav className="flex flex-col justify-center items-center gap-8">
-          {links.map((link, index) => (
+          {links.map((link) => (
             <Link
               href={link.path}
-              key={index}
-              onClick={link.isLogo ? undefined : handleLinkClick} // Don't close sheet on logo click
+              key={link.path}
+              onClick={handleLinkClick} // Close the sheet when a link is clicked
               className={`${
                 link.path === pathname &&
                 "text-amber-700 border-b-2 border-amber-700"
