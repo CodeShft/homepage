5c4ed62fd919935a389c69400b2a2176cd04a46c
CodeShft <nileceyazilim@gmail.com>
2024-11-20 00:52:28 +0300

    modified

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,8 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-center space-y-8 xl:space-y-0 xl:space-x-16 xl:pt-8 xl:pb-16">
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Cat</span>
             <h1 className="h1 mb-6  text-amber-700">
               Hello I am Lokum <br />
             </h1>
@@ -16,7 +15,7 @@ const Home = () => {
               dried cat food and sleep. I also get along well with other cats.
             </p>
           </div>
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <div className="order-1 xl:order-none">
             <Photo />
           </div>
         </div>
--- components/ui/Stats.jsx
+++ components/ui/Stats.jsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 const stats = [
   {
     num: 1,
-    text: "6 Years old",
+    text: "6 Years old a cat",
   },
   {
     num: 2,
@@ -17,30 +17,30 @@ const stats = [
   },
   {
     num: 4,
-    text: "A neutered cat",
+    text: "Neutered cat",
   },
 ];
 
 const Stats = () => {
   return (
-    <section className="pt-3 pb-12 xl:pt-0 xl:pb-0">
+    <section className="pt-0 pb-0 xl:pt-0 xl:pb-0 -mt-4 xl:-mt-8">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-8 max-w-[80vw] mx-auto xl:max-w-none text-green-700">
+        <div className="flex flex-row flex-wrap justify-center items-center gap-10 max-w-[90vw] mx-auto xl:max-w-none text-green-800">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex items-center justify-start gap-6 sm:gap-8 xl:gap-4" // Burada gap'i artırdım
+                className="flex flex-col items-center text-center gap-1 p-2 w-24 h-24 justify-center bg-zinc-900 rounded-xl hover:bg-zinc-800 transition-all duration-300 shadow-lg"
                 key={index}
               >
-                {/* Sayı kısmı */}
-                <CountUp
-                  end={item.num}
-                  duration={5}
-                  delay={2}
-                  className="text-4xl sm:text-5xl xl:text-6xl font-extrabold mt-[-30px]" // Sayıyı yukarıya taşıdık
-                />
-                {/* Metin kısmı */}
-                <p className="text-sm sm:text-base xl:text-lg leading-snug text-white/80 mt-[-30px]">
+                <div className="h-8 flex items-center">
+                  <CountUp
+                    end={item.num}
+                    duration={5}
+                    delay={2}
+                    className="text-2xl xl:text-3xl font-bold text-amber-500"
+                  />
+                </div>
+                <p className="text-xs xl:text-sm text-white/90 font-medium">
                   {item.text}
                 </p>
               </div>
