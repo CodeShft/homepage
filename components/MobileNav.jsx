b02112245499e5815253338b0c79e177a0c97d26
CodeShft <nileceyazilim@gmail.com>
2024-11-19 21:05:42 +0300

    .

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -30,30 +30,29 @@ const MobileNav = () => {
       >
         <CiMenuFries className="text-[32px] text-amber-700" />
       </SheetTrigger>
-      <SheetContent side="right" className="w-[300px] sm:w-[400px] bg-white">
+      <SheetContent side="right" className="w-[300px] sm:w-[400px] bg-primary transition-transform duration-300 ease-in-out">
         {/* Erişilebilirlik için SheetTitle başlığı eklendi */}
         <SheetTitle className="sr-only">Mobil Menü</SheetTitle>
 
         {/* Logo */}
         <div className="flex justify-center items-center h-20 border-b">
           <Link href="/" onClick={handleLinkClick}>
-            <h1 className="text-2xl sm:text-3xl font-semibold">
-              Lokum<span className="text-amber-700">.</span>
+            <h1 className="text-2xl sm:text-3xl font-semibold"><span className="text-amber-700">Lokum.</span>
             </h1>
           </Link>
         </div>
 
         {/* Navigation */}
         <nav className="flex flex-col items-center mt-8">
-          {links.map((link) => (
+        {links.map((link) => (
             <Link
               href={link.path}
               key={link.path}
               onClick={handleLinkClick} 
-              className={`w-full py-4 text-center text-lg sm:text-xl capitalize hover:bg-amber-50 transition-all ${
+              className={`w-full py-4 text-center text-lg sm:text-xl capitalize hover:bg-amber-700 transition-all ${
                 link.path === pathname
                 ? "text-amber-700 font-medium"
-                : "text-gray-600"
+                : "text-white"
               }`}
             >
               {link.name}
