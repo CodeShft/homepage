0697b5af3bd59972cc00430430330d3516618f2b
CodeShft <nileceyazilim@gmail.com>
2024-11-25 20:02:40 +0300

    .

--- components/ui/Stats.jsx
+++ components/ui/Stats.jsx
@@ -23,24 +23,24 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section className="pt-2 pb-2 xl:pt-6 xl:pb-4 -mt-8 xl:-mt-12">
+    <section className="pt-4 sm:pt-2 pb-2 xl:pt-4 xl:pb-4 -mt-12 xl:-mt-16">
       <div className="container mx-auto">
-        <div className="flex flex-row flex-wrap justify-start items-center gap-10 max-w-[90vw] ml-20 xl:ml-36 xl:max-w-none text-green-800">
+        <div className="flex flex-row justify-start items-center gap-3 sm:gap-6 pl-6 sm:pl-16 md:pl-24 lg:pl-32">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex flex-col items-center text-center gap-1 p-2 w-24 h-24 justify-center bg-zinc-900 rounded-xl hover:bg-zinc-800 transition-all duration-300 shadow-lg"
+                className="flex flex-col items-center text-center gap-1 p-2 w-[75px] sm:w-24 h-[80px] sm:h-24 justify-center bg-zinc-900 rounded-xl hover:bg-zinc-800 transition-all duration-300 shadow-lg"
                 key={index}
               >
-                <div className="h-8 flex items-center">
+                <div className="h-6 sm:h-8 flex items-center">
                   <CountUp
                     end={item.num}
                     duration={5}
                     delay={2}
-                    className="text-2xl xl:text-3xl font-bold text-amber-500"
+                    className="text-xl sm:text-2xl xl:text-3xl font-bold text-amber-500"
                   />
                 </div>
-                <p className="text-xs xl:text-sm text-white/90 font-medium">
+                <p className="text-[10px] sm:text-xs xl:text-sm text-white/90 font-medium">
                   {item.text}
                 </p>
               </div>
